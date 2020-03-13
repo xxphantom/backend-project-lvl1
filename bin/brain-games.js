c2fb5e7c31aa60d4a6b645cfde4caecb62c0f209
Дмитрий Герасимов <up@websector.ru>
2020-03-13 16:49:11 +0300

    Add Greetings logic by readlineSync package

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+import inputName from '../src/index.js'; 
 console.log('Welcome to the Brain Games');
+inputName();
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const inputName = () => {
+    const userName = readlineSync.question(`May I have your name?\n> `);
+    console.log(`Hello, ${userName}!`)
+};
+
+export default inputName;
+
