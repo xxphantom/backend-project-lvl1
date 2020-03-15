2baa3018263818602c2df37e296db45fe6e78c82
Дмитрий Герасимов <up@websector.ru>
2020-03-15 20:04:38 +0300

    Add game IsEven

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import inputName from '../src/index.js';
+import greetings from '../src/index.js';
 
-console.log('Welcome to the Brain Games\nMay I have your name?');
-console.log(`Hello, ${inputName()}!`);
+greetings();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import readlineSync from 'readline-sync';
 
-const inputName = () => {
+export const userInput = () => {
   const userName = readlineSync.question('> ');
   return userName;
 };
 
-export default inputName;
+export const greetings = () => {
+  console.log('Welcome to the Brain Games\nMay I have your name?');
+  const userName = userInput();
+  console.log(`Hello, ${userName}!`);
+  return (userName);
+};
