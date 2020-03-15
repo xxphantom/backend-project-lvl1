1ea8023b475635519203b79788a5f5baf1e9a09b
Дмитрий Герасимов <up@websector.ru>
2020-03-15 17:41:56 +0300

    some refactoring

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import inputName from '../src/index.js';
 
-console.log('Welcome to the Brain Games');
-inputName();
+console.log('Welcome to the Brain Games\nMay I have your name?');
+console.log(`Hello, ${inputName()}!`);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const inputName = () => {
-  const userName = readlineSync.question('May I have your name?\n> ');
-  console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('> ');
+  return userName;
 };
 
 export default inputName;
