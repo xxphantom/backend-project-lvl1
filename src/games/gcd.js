3fe84b43ac7ab72b7b4facef64877aafeeb01ee0
Дмитрий Герасимов <up@websector.ru>
2020-03-17 16:37:20 +0300

    Add gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import { gameFlow, random } from '../index.js';
+
+const gcd = (a, b) => {
+  if (b === 0) {
+    return (Math.abs(a));
+  }
+  return gcd(b, a % b);
+};
+
+export default () => {
+  const getGameParam = () => {
+    const gameTask = 'Find the greatest common divisor of given numbers.';
+    const number1 = random(1, 250);
+    const number2 = random(1, 250);
+    const question = `${number1} ${number2} `;
+    const answer = gcd(number1, number2).toString();
+
+    return [gameTask, question, answer];
+  };
+
+  gameFlow(getGameParam);
+};
