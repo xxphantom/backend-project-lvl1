b73ab0ecc1037baa8cf67ccda6af77f3b06e688c
Дмитрий Герасимов <up@websector.ru>
2020-03-18 21:46:37 +0300

    Add game prime

---
+++ src/games/prime.js
@@ -0,0 +1,21 @@
+import { gameFlow, random } from '../index.js';
+
+export default () => {
+  const getGameParam = () => {
+    const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    const question = random(0, 101);
+
+    const isPrime = (num) => {
+      for (let i = 2; i <= Math.sqrt(num); i += 1) {
+        if (num % i === 0) return false;
+      }
+      return true;
+    };
+
+    const answer = isPrime(question) ? 'yes' : 'no';
+
+    return [gameTask, question, answer];
+  };
+
+  gameFlow(getGameParam);
+};
