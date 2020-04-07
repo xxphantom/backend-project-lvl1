d2a32b6c287606c7ac06277b9998e0adab51157c
Dmitriy Gerasimov <up@websector.ru>
2020-04-07 18:15:03 +0300

    separate greeting from name request

--- src/games/welcome.js
+++ src/games/welcome.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const startGreeting = () => {
-  const userName = readlineSync.question('Welcome to the Brain Games\nMay I have your name? ');
+  console.log('Welcome to the Brain Games');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import readlineSync from 'readline-sync';
 const maxNumberRounds = 3;
 
 const runGameFlow = (gameTask, getGameParam) => {
-  const userName = readlineSync.question('Welcome to the Brain Games\nMay I have your name? ');
+  console.log('Welcome to the Brain Games');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(gameTask);
 
