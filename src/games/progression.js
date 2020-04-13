383a969fbc9332f6d2199361de1112580044f324
Dmitriy Gerasimov <up@websector.ru>
2020-04-13 20:23:14 +0300

    some refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,15 +2,26 @@ import runGameFlow from '../index.js';
 import random from '../utils.js';
 
 const gameTask = 'What number is missing in the progression?';
+const progressionLength = 10;
+const progressionMaxStep = 9;
+const progressionMaxFirstItem = 15;
+
+const getProgression = (firstItem, step, length) => {
+  const progression = [firstItem];
+  for (let i = 0; i < length - 1; i += 1) {
+    progression.push(progression[i] + step);
+  }
+  return progression;
+};
 
 const getGameData = () => {
-  const randomStep = random(1, 9);
-  const randomIndex = random(0, 9);
-  const progrLength = 10;
-  const progression = Array(progrLength).fill('').map((num, i) => randomStep * (i + 1));
+  const step = random(1, progressionMaxStep);
+  const firstItem = random(1, progressionMaxFirstItem);
+  const hiddenItemIndex = random(0, progressionLength - 1);
+  const progression = getProgression(firstItem, step, progressionLength);
 
-  const answer = progression[randomIndex].toString();
-  progression[randomIndex] = '..';
+  const answer = progression[hiddenItemIndex].toString();
+  progression[hiddenItemIndex] = '..';
   const question = progression.join(' ');
 
   return [question, answer];
