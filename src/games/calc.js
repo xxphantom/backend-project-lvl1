2f033cb04eac6e0042edf5ddb181a99a97c499f4
Dmitriy Gerasimov <up@websector.ru>
2020-04-13 15:54:09 +0300

    some refactoring

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,26 +5,21 @@ const gameTask = 'What is the result of the expression?';
 const operators = ['*', '+', '-'];
 
 const calculate = (operator, number1, number2) => {
-  let result = '';
   switch (operator) {
     case '*':
-      result = (number1 * number2);
-      break;
+      return (number1 * number2);
     case '-':
-      result = (number1 - number2);
-      break;
+      return (number1 - number2);
     case '+':
-      result = (parseInt(number1, 10) + parseInt(number2, 10));
-      break;
+      return (number1 + number2);
     default:
       throw new Error(`Incorrect operator: ${operator}`);
   }
-  return result;
 };
 
 const getGameData = () => {
-  const randomIndex = random(0, operators.length - 1);
-  const operator = operators[randomIndex];
+  const operatorIndex = random(0, operators.length - 1);
+  const operator = operators[operatorIndex];
   const number1 = random(0, 100);
   const number2 = random(0, 100);
   const question = `${number1} ${operator} ${number2} `;
